41b3dfd92e91f749865b54842414fcb673d0024a
abhi-2605 <abhishekmenon2605@gmail.com>
2021-04-23 22:15:26 +0530

    first commit

--- Backend/model/Accountdata.js
+++ Backend/model/Accountdata.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 // connect to cloud Database
-mongoose.connect("mongodb+srv://Anjali_Peter:anju_mlab@libapp.ggjw4.mongodb.net/LibraryApp?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://abhi_abhi:anju_mlab@libapp.ggjw4.mongodb.net/LibraryApp?retryWrites=true&w=majority");
 
 // connect to manually created Database
 // mongoose.connect("mongodb://localhost:27017/library");
@@ -11,14 +11,14 @@ var Schema = mongoose.Schema;
 
 // define Schema structure for an user account
 var AccountSchema = new Schema({
-    name : String,
-    email : String,
-    username : String,
-    password : String
+    name: String,
+    email: String,
+    username: String,
+    password: String
 });
 
 // create model
-var Accountdata = mongoose.model("accountdata",AccountSchema);
+var Accountdata = mongoose.model("accountdata", AccountSchema);
 
 // export model
 module.exports = Accountdata;
--- Backend/model/Authordata.js
+++ Backend/model/Authordata.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // connect to cloud Database
-mongoose.connect("mongodb+srv://Anjali_Peter:anju_mlab@libapp.ggjw4.mongodb.net/LibraryApp?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://abhi_abhi:anju_mlab@libapp.ggjw4.mongodb.net/LibraryApp?retryWrites=true&w=majority");
 
 // connect to manually created Database
 // mongoose.connect("mongodb://localhost:27017/library");
@@ -11,15 +11,15 @@ const Schema = mongoose.Schema;
 
 // define Schema structure for an author
 const AuthorSchema = new Schema({
-    author : String,
-    book : String,
-    genre : String,
-    image : String,
-    about : String
+    author: String,
+    book: String,
+    genre: String,
+    image: String,
+    about: String
 });
 
 // create model
-var Authordata = mongoose.model("authordata",AuthorSchema);
+var Authordata = mongoose.model("authordata", AuthorSchema);
 
 // export model
 module.exports = Authordata;
